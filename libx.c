a6cb2e8a496005dedb7bc98c3bcd6a058722b46c
jesperkha <hammerjesp@gmail.com>
2024-05-31 14:21:50 +0200

    arena

--- libx.c
+++ libx.c
@@ -2,41 +2,85 @@
 #include <windows.h>
 #include <stdlib.h>
 
+#define xdefaultAlloc(size) (malloc(size))
+#define xdefaultFree(p) (free(p))
+
 static char *error_msgs[] = {
 	[NO_ERROR] = "No error",
 	[ERR_FILE_READ] = "Failed to read from file",
+	[ERR_NO_MEMORY] = "Out of memory",
 };
 
-string XError(error e)
+char *XError(error e)
 {
 	return error_msgs[e];
 }
 
-error XReadFile(const char *filepath, File *f)
+Arena XArenaNew(int size)
 {
+	void *m = xdefaultAlloc(size);
+	if (m == NULL)
+		return (Arena){.err = ERR_NO_MEMORY};
+
+	return (Arena){
+		.err = NO_ERROR,
+		.freed = false,
+		.memory = m,
+		.pos = 0,
+		.size = size,
+	};
+}
+
+void *XArenaAlloc(Arena *a, int size)
+{
+	if (a->pos + size >= a->size)
+	{
+		a->err = ERR_NO_MEMORY;
+		return NULL;
+	}
+
+	void *p = a->memory + a->pos;
+	a->pos += size;
+	return p;
+}
+
+void XArenaFree(Arena a)
+{
+	xdefaultFree(a.memory);
+}
+
+File XReadFile(const char *filepath)
+{
+	File f = {0};
+
 	// Open file. EditorOpenFile does not create files and fails on file-not-found
 	HANDLE file = CreateFileA(filepath, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (file == INVALID_HANDLE_VALUE)
-		return ERR_FILE_READ;
+		goto return_error;
 
 	// Get file size and read file contents into string buffer
 	DWORD bufSize = GetFileSize(file, NULL) + 1;
 	DWORD read;
-	char *buffer = malloc(bufSize);
+	char *buffer = xdefaultAlloc(bufSize);
 	if (!ReadFile(file, buffer, bufSize, &read, NULL))
 	{
 		CloseHandle(file);
-		return ERR_FILE_READ;
+		goto return_error;
 	}
 
 	CloseHandle(file);
 	buffer[bufSize - 1] = 0;
-	f->size = bufSize - 1;
-	f->data = buffer;
-	return NO_ERROR;
+	f.size = bufSize - 1;
+	f.data = buffer;
+	strncpy(f.filepath, filepath, 260);
+	return f;
+
+return_error:
+	f.err = ERR_FILE_READ;
+	return f;
 }
 
 void XFreeFile(File f)
 {
-	free(f.data);
+	xdefaultFree(f.data);
 }
--- libx.h
+++ libx.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <stdbool.h>
+#include <string.h>
+
 #define u8 unsigned char
 #define u16 unsigned short
 #define u32 unsigned int
@@ -10,27 +13,55 @@
 #define i64 long
 #define f32 float
 
-typedef char *string; // A string can be assumed to be null-terminated
+// Defer a function call or expression to be evaluated after the preceding block.
+#define defer(f) for (int __defer_i = 0; !__defer_i; (f, __defer_i++))
 
 // Error code. Get message with XError().
 typedef enum error
 {
     NO_ERROR,
     ERR_FILE_READ,
+    ERR_NO_MEMORY,
 } error;
 
-// Returns string representation of error code.
-string XError(error e);
+typedef struct String
+{
+    char *str;
+    int length;
+    error err;
+} String;
+
+// Makes a new string
+#define str(s) ((String){.str = s, .length = strlen(s), .err = 0})
+
+typedef struct Arena
+{
+    void *memory;
+    int size;
+    int pos;
+    bool freed;
+    error err;
+} Arena;
 
 typedef struct File
 {
     char filepath[260];
     int size;
     char *data;
+    error err;
 } File;
 
-// Reads file and writes to dest. Remember to call XFreeFile().
-error XReadFile(const char *filepath, File *dest);
+// Returns string representation of error code.
+char *XError(error e);
 
-// Frees file data pointer, not file object if allocated.
-void XFreeFile(File f);
+// Can only be used with object containing an 'err' field.
+#define Ok(o) (o.err == 0)
+
+Arena XArenaNew(int size);             // Allocates new arena with given size. Sets Arena.err on failure.
+void *XArenaAlloc(Arena *a, int size); // Returns NULL on failure and sets Arena.err.
+void XArenaFree(Arena a);              // Frees internal memory pointer.
+
+// Reads file and returns it. Sets error in File.err on failure.
+// Uses default allocator, remember to call XFreeFile().
+File XReadFile(const char *filepath);
+void XFreeFile(File f); // Use only when not allocated with Arena
